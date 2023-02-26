43897224f4520df472b022a5fd72acc10409995f
courtamour <novikova19992806@gmail.com>
2023-02-26 14:23:43 +0300

    feat: add function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    result;
+    minD;
+    maxD;
+    constructor() { }
 
     setRange(min, max) {
-
+        this.minD = min;
+        this.maxD = max;
     }
 
     guess() {
-
+        this.result = Math.ceil((this.minD + this.maxD) / 2);
+        console.log(this.result);
+        return this.result;
     }
 
     lower() {
-
+        this.maxD = this.result;
     }
 
     greater() {
-
+        this.minD = this.result;
     }
 }
 
